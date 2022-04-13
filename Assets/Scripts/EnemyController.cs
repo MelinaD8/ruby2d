330c84b7350075eab7afac7028f7ff8f8d141973
MelinaD8 <97529281+MelinaD8@users.noreply.github.com>
2022-04-13 09:01:09 -0400

    part 14

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,14 +7,14 @@ public class EnemyController : MonoBehaviour
     public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2d;
     float timer;
     int direction = 1;
     
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
     }
 
@@ -33,7 +33,7 @@ public class EnemyController : MonoBehaviour
 
 void FixedUpdate()
     {
-        Vector2 position = rigidbody2D.position;
+        Vector2 position = rigidbody2d.position;
         
         if (vertical)
         {
@@ -44,7 +44,7 @@ void FixedUpdate()
             position.x = position.x + Time.deltaTime * speed * direction;;
         }
         
-        rigidbody2D.MovePosition(position);
+        rigidbody2d.MovePosition(position);
     }
 
     void OnCollisionEnter2D(Collision2D other)
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -19,6 +19,7 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float vertical;
     float horizontal;
+    Vector2 lookDirection = new Vector2(1,0);
 
     // Start is called before the first frame update
     void Start()
@@ -41,6 +42,14 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+        if (Input.GetKeyDown(KeyCode.X))
+{
+    RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+    if (hit.collider != null)
+    {
+        Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+    }
+}
     }
     
 void FixedUpdate()
